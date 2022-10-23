e1c9ce4719825dadcd5b2819d01fc00fc00fa6c2
betül nar <betnarr@hotmail.com>
2022-10-23 23:41:48 +0300

    aaa

---
+++ java/1.day.java
@@ -0,0 +1,19 @@
+class first {
+    public static void main(String[] args) {
+        int[] numbers = { 9, 4, 5, 2, 3, 6, 1, 4, 7, 2, 2 };
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            //if (numbers[i] > max) {
+                max = i;
+
+           // }
+
+            // if (numbers[i]%2==0){
+            // System.out.println(numbers[i]);
+        }
+        System.out.println("EN BÜYÜK SAYI " + max);
+
+        // System.out.println(numbers[i]);
+    }
+
+}
